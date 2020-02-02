bf9b5aebe55e4933128e5f057adf767d937976de
Wolodja <wolodjawaskovych@i.ua>
2020-02-02 14:21:41 +0100

    Created Category entity. ManyToMany relation.

--- src/main/java/guru/springframework/recipe_app/domain/Recipe.java
+++ src/main/java/guru/springframework/recipe_app/domain/Recipe.java
@@ -30,6 +30,12 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes note;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
@@ -124,5 +130,13 @@ public class Recipe {
 
     public void setNote(Notes note) {
         this.note = note;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
