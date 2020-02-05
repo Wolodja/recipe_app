974f9812c9f6a3cd13c04a7cc7edf77a4352356c
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-02-05 07:59:38 +0100

    HashCode problem fix

--- src/main/java/guru/springframework/recipe_app/services/impl/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/recipe_app/services/impl/RecipeServiceImpl.java
@@ -3,11 +3,13 @@ package guru.springframework.recipe_app.services.impl;
 import guru.springframework.recipe_app.domain.Recipe;
 import guru.springframework.recipe_app.repositories.RecipeRepository;
 import guru.springframework.recipe_app.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,6 +21,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
