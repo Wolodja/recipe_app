5a5dc99f8fb626a110ac7de0d2593e4bffb0b1f3
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-02-07 15:09:09 +0100

    Recipe Service test added

---
+++ src/test/java/guru/springframework/recipe_app/services/impl/RecipeServiceImplTest.java
@@ -0,0 +1,47 @@
+package guru.springframework.recipe_app.services.impl;
+
+import guru.springframework.recipe_app.domain.Recipe;
+import guru.springframework.recipe_app.repositories.RecipeRepository;
+import guru.springframework.recipe_app.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeSet);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
