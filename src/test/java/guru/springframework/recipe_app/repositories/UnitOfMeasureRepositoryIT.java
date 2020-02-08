bac62e9e9b2bf904a9c048ece82e80bc258ff450
Wolodja <wolodjawaskovych@i.ua>
2020-02-08 13:02:00 +0100

    Integration tests added

---
+++ src/test/java/guru/springframework/recipe_app/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,33 @@
+package guru.springframework.recipe_app.repositories;
+
+import guru.springframework.recipe_app.domain.UnitOfMeasure;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    @DirtiesContext
+    void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", unitOfMeasure.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", unitOfMeasure.get().getDescription());
+    }
+}
