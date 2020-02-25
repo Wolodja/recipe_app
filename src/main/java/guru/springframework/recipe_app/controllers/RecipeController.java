2109e4e99a9ea52219b417cf07ea136026bbc0b0
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-02-25 17:02:44 +0100

    Recipe form and post method added

--- src/main/java/guru/springframework/recipe_app/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipe_app/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package guru.springframework.recipe_app.controllers;
 
+import guru.springframework.recipe_app.commands.RecipeCommand;
 import guru.springframework.recipe_app.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,11 +19,25 @@ public class RecipeController {
     }
 
     @RequestMapping("/recipe/show/{id}")
-    public String showById(@PathVariable String id, Model model){
+    public String showById(@PathVariable String id, Model model) {
 
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
 
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
