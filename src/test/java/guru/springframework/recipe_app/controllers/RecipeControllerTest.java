93b19b1f01921f59c161bd9b17d7458fabbada0d
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-03-17 10:41:42 +0100

    Added handler for format exceptions

--- src/test/java/guru/springframework/recipe_app/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/recipe_app/controllers/RecipeControllerTest.java
@@ -65,6 +65,16 @@ public class RecipeControllerTest {
     }
 
     @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        when(recipeService.findById(anyLong())).thenThrow(NumberFormatException.class);
+        mockMvc.perform(get("/recipe/sfsdfsf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
+    @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
 
