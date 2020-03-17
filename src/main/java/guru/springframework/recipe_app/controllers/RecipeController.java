db0ea1873b7d9062b451bcf4980a5013b6a06d9b
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-03-17 09:56:50 +0100

    Exception Handler added

--- src/main/java/guru/springframework/recipe_app/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipe_app/controllers/RecipeController.java
@@ -1,14 +1,19 @@
 package guru.springframework.recipe_app.controllers;
 
 import guru.springframework.recipe_app.commands.RecipeCommand;
+import guru.springframework.recipe_app.exceptions.NotFoundException;
 import guru.springframework.recipe_app.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Slf4j
@@ -53,5 +58,17 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
--- src/main/java/guru/springframework/recipe_app/exceptions/NotFoundException.java
+++ src/main/java/guru/springframework/recipe_app/exceptions/NotFoundException.java
@@ -1,11 +1,8 @@
 package guru.springframework.recipe_app.exceptions;
 
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @NoArgsConstructor
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
